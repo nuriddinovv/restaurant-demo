6fed59b8bb7f3951a8e234231686e6f4f4126852
Rahkmatjon Nuriddinov <blackscarp1onoff@gmail.com>
2024-11-09 22:16:38 +0500

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
-
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -12,75 +10,75 @@ export default function Home() {
   return (
     <div className="font-one">
       <div className="font-two pt-[120px] pb-6">
-        <p className=" text-4xl md:text-6xl lg:text-8xl text-white container text-center">
-          Welcome Demà Restaurant
+        <p className="container text-center text-white text-4xl md:text-6xl lg:text-8xl">
+          Welcome to DeMà Restaurant
         </p>
       </div>
-      <div className=" py-10">
-        <div className="container grid grid-cols-1 mx-auto md:grid-cols-2">
+      <div className="py-10">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2">
           <Image
-            src={"/sect_one.jpg"}
-            alt={"a"}
+            src="/sect_one.jpg"
+            alt="Section one image"
             width={500}
             height={400}
-            className="rounded-lg border-[15px] mx-auto"
+            className="mx-auto rounded-lg border-[15px]"
           />
-          <p className="m-auto text-gray-400 text-2xl w-[60%] ">
+          <p className="m-auto w-[60%] text-2xl text-gray-400">
             <span className="text-mainColor">DeMà</span> Restaurant's commitment
             begins with authentic and memorable Italian dishes from the
             southeast of Italy in a{" "}
             <span className="text-mainColor">"rustic euro"</span> modern yet
             comfortable dining ambience for a quality food experience with the
-            choice of dining in, take-out and delivery.
+            choice of dining in, take-out, and delivery.
           </p>
         </div>
       </div>
       <div className="py-10">
-        <div className="container grid grid-cols-1 mx-auto md:grid-cols-2">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2">
           <div className="m-auto w-[60%]">
-            <p className=" font-two text-6xl mb-10 text-white text-center">
-              Demà Restaurant
+            <p className="text-center text-6xl text-white font-two mb-10">
+              DeMà Restaurant
             </p>
-            <p className="text-gray-400 text-2xl mb-7">
+            <p className="text-2xl text-gray-400 mb-7">
               Italian cuisine is popular in Dubai because it's delicious,
-              authentic and healthy. The Petrone Family, originally from Bari,
+              authentic, and healthy. The Petrone Family, originally from Bari,
               the South of Italy, brings traditional recipes passed down through
               generations to your plate in Dubai, UAE.
             </p>
-            <p className="text-gray-400 text-2xl">
+            <p className="text-2xl text-gray-400">
               DeMà brings you a diversified unique Italian experience to your
-              table using handmade pasta, the very best local seasonal produce
-              and the finest South- East Italian style recipes.
+              table using handmade pasta, the very best local seasonal produce,
+              and the finest South-East Italian style recipes.
             </p>
           </div>
           <Image
-            src={"/square.jpg"}
-            alt={"a"}
+            src="/square.jpg"
+            alt="Square image"
             width={400}
             height={400}
-            className="rounded-lg border-[15px] mx-auto "
+            className="mx-auto rounded-lg border-[15px]"
           />
         </div>
       </div>
       <div className="container py-10">
         <Image
-          src={"/sect_two.jpg"}
-          alt="a"
+          src="/sect_two.jpg"
+          alt="Section two image"
           width={700}
           height={500}
           className="mx-auto rounded-lg border-[15px] mb-10"
         />
         <div className="grid grid-cols-2 gap-32">
-          <div className="">
-            <p className="text-gray-400 text-2xl">
+          <div>
+            <p className="text-2xl text-gray-400">
               We provide our patrons with delicious traditional and
               straightforward dishes and exquisite delectables such as the
               famous square style thin pizza, arancini, parmigiana di melansana,
-              panzerotto, gnocchi al forno and more, all cooked to order.
+              panzerotto, gnocchi al forno, and more, all cooked to order.
             </p>
           </div>
-          <div className="">
-            <p className="text-gray-400 text-2xl">
+          <div>
+            <p className="text-2xl text-gray-400">
               To complete your dining experience, choose a sweet and light
               Italian dessert.
             </p>
